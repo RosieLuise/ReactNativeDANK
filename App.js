cebe1a913dcbc29caf96517e66d5fe7395971c32
cjmcclure1337 <62575120+cjmcclure1337@users.noreply.github.com>
2021-04-24 10:53:00 -0500

    Running Home without Navigation

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from "./components/HomeComponent";
+import Contact from "./components/ContactComponent";
 
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
