6c623599d997a483fd7089f53c6013f6b09a17c4
cjmcclure1337 <62575120+cjmcclure1337@users.noreply.github.com>
2021-04-25 16:34:46 -0500

    Basic Contact Form

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {View, Text} from "react-native";
-import {Card} from "react-native-elements"
+import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Alert } from 'react-native';
+import {Card, Input, Icon} from "react-native-elements";
+
 
 class Contact extends Component {
     constructor(props) {
@@ -13,12 +14,34 @@ class Contact extends Component {
     
     render() {
         return(
-            <Card   
-            featuredTitle="Contact title"
-            image={{uri: "../assets/favicon.png"}}
-        >
-            <Text style={{margin: 10}}>This is the contact component</Text>
-        </Card>
+            <ScrollView>
+                <View>
+                    <Card title="Contact Information" wrapperStyle={{margin: 20}}>
+                        <Text>4740 N Western Ave</Text>
+                        <Text>Chicago, IL 60625</Text>
+                        <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                        <Text>Phone: (773) 561-9181</Text>
+                    </Card>
+                </View>
+                <View>
+                    <Card title="Contact Information" wrapperStyle={{margin: 20}}>
+                        <Text>Have a question? Feel free to submit it below:</Text>
+                        <Input 
+                            placeholder="Name"
+                        />
+                        <Input 
+                            placeholder="Email"
+                        />
+                        <Input 
+                            placeholder="Comments"
+                            multiline
+                            numberOfLines={4}
+                            style={{textAlignVertical:"top"}}
+                        />
+                        <Button title="submit" />
+                    </Card>
+                </View>
+            </ScrollView>
         )
     }
 }
